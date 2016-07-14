d51fa865eac359b7acaa51763de03dfc5485fb45
jackw2050 <jackw2050@gmail.com>
2016-07-14 14:55:08 -0500

    minor cleanup

--- Terminal.cs
+++ Terminal.cs
@@ -880,8 +880,8 @@ namespace SerialPortTerminal
                         AutoStartForm.crossGyroStatusLabel.ForeColor = Color.Red;
                         AutoStartForm.crossGyroStatusLabel.Text = "Not Ready";
                     }
-                    { }
-                    DataStatusForm.textBox19.Text = "Bad";
+                    
+                    DataStatusForm.textBox19.Text = "Not Ready";
                 }
                 if (MeterStatus.xGyro_Fog == 0)
                 {
@@ -900,9 +900,9 @@ namespace SerialPortTerminal
                         AutoStartForm.longGyroStatusLabel.Text = "Not Ready";
                     }
 
-                    DataStatusForm.textBox20.Text = "Bad";
+                    DataStatusForm.textBox20.Text = "Not Ready";
                 }
-                if (meter_status == 0)
+                if (MeterStatus.meterHeater == 0)
                 {
                     if (AutoStartForm.Visible)
                     {
@@ -911,7 +911,7 @@ namespace SerialPortTerminal
                     }
                     if (DataStatusForm.Visible)
                     {
-                        DataStatusForm.textBox21.Text = "OK";
+                        DataStatusForm.textBox21.Text = "Ready";
                     }
                 }
                 else
@@ -2193,21 +2193,12 @@ namespace SerialPortTerminal
         // Start data collection.  Send command 1 for meter to start data stream
         private void button2_Click(object sender, EventArgs e)
         {
-            //   OpenPort();
-            // Convert the user's string of hex digits (ex: B4 CA E2) to a byte array
+
             byte[] data = { 0x01, 0x08, 0x09 };                                        //HexStringToByteArray(txtSendData.Text);
 
-            // Send the binary data out the port
-            //    comport.Write(data, 0, data.Length);
-
-            //         _timer1.Interval = 1000; //  (5000 - DateTime.Now.Millisecond);
-            //         _timer1.Enabled = true;
-            // Show the hex digits on in the terminal window
             Log(LogMsgType.Outgoing, ByteArrayToHexString(data) + "\n");
         }
 
-        //  This will be sendData later will full command byte array and checksum.
-        //  Not needed until I implement sending commands, settings etc.
         private void button3_Click(object sender, EventArgs e)
         {
             byte checkSum = 0;
@@ -2255,102 +2246,6 @@ namespace SerialPortTerminal
             UserDataForm.dataFileTextBox.Text = fileName;
         }
 
-        // comment for now
-        /*
-
-        public void RecordDataToFile(string fileOperation, int d)
-        {
-            // fileOperation  0 - open,  1 - append, 2 - close
-            string delimitor = ",";
-
-            switch (frmTerminal.fileType)
-            {
-                case "csv":
-                    delimitor = ",";
-                    break;
-
-                case "tsv":
-                    delimitor = "\t";
-                    break;
-
-                case "txt":
-                    delimitor = " ";
-                    break;
-
-                default:
-                    break;
-            }
-
-            if (frmTerminal.gravityFileName == null)
-            {
-                DateTime now = DateTime.Now;
-                // String.Format("{0:dds}", now);
-                //  fileName = "C:\\Ultrasys\\Data\\" + "GravityData" + now.ToString("yyyyMMddHHmmsstt") + ".csv";
-                //  fileName = frmTerminal.filePath;
-            }
-            else
-            {
-                //  fileName = "C:\\Ultrasys\\Data\\" + frmTerminal.gravityFileName;
-            }
-
-            if (fileOperation == "Open")
-            {
-                try
-                {
-                    using (StreamWriter writer = File.CreateText(frmTerminal.fileName))
-                    {
-                        string header = "Date" + delimitor + "Gravity" + delimitor + "Spring Tension" + delimitor
-                            + "Cross coupling" + delimitor + "Raw Beam" + delimitor + "VCC or CML" + delimitor + "AL"
-                            + delimitor + "AX" + delimitor + "VE" + delimitor + "AX2 or CMX" + delimitor + "XACC2" + delimitor
-                            + "LACC2" + delimitor + "XACC" + delimitor + "LACC" + delimitor + "Parallel Port" + delimitor
-                            + "Platform Period" + delimitor + "AUX1" + delimitor + "AUX2" + delimitor + "AUX3" + delimitor + "AUX4";
-                        writer.WriteLine(header);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.ToString());
-                }
-            }
-            else if (fileOperation == "Append")
-            {
-                string myString = Convert.ToString(d.Date) + delimitor + Convert.ToString(d.Gravity) + delimitor + Convert.ToString(d.SpringTension)
-                     + delimitor + Convert.ToString(d.CrossCoupling) + delimitor + Convert.ToString(d.RawBeam) + delimitor + Convert.ToString(d.VCC)
-                     + delimitor + Convert.ToString(d.AL) + delimitor + Convert.ToString(d.AX) + delimitor + Convert.ToString(d.VE)
-                     + delimitor + Convert.ToString(d.AX2) + delimitor + Convert.ToString(d.XACC2) + delimitor + Convert.ToString(d.LACC2)
-                     + delimitor + Convert.ToString(d.XACC) + delimitor + Convert.ToString(d.LACC) + delimitor + Convert.ToString(d.ParallelData)
-                     + delimitor + Convert.ToString(d.AUX1) + delimitor + Convert.ToString(d.AUX2) + delimitor + Convert.ToString(d.AUX3)
-                     + delimitor + Convert.ToString(d.AUX4);
-
-                 string myString = Convert.ToString(MeterData.dataTime) + delimitor + Convert.ToString(MeterData.data4[2] * ConfigData.beamScale)
-                   +delimitor + Convert.ToString(MeterData.data4[3]) + delimitor + Convert.ToString(MeterData.data4[4])
-                   + delimitor + Convert.ToString(MeterData.data1[5]) + delimitor + Convert.ToString(MeterData.data1[6])
-                   + delimitor + Convert.ToString(MeterData.data1[7]) + delimitor + Convert.ToString(MeterData.data1[8])
-                   + delimitor + Convert.ToString(MeterData.data1[9]) + delimitor + Convert.ToString(MeterData.data1[10])
-                   + delimitor + Convert.ToString(MeterData.data1[11]) + delimitor + Convert.ToString(MeterData.data1[12])
-                   + delimitor + Convert.ToString(MeterData.data1[13]) + delimitor + Convert.ToString(MeterData.data1[14])
-                   + delimitor + "I4PAR" + delimitor + "APERX * 1.0E6"
-
-                    + delimitor + Convert.ToString(MeterData.data1[15]) + delimitor + Convert.ToString(MeterData.data1[16])
-                    + delimitor + Convert.ToString(MeterData.data1[17]) + delimitor + Convert.ToString(MeterData.data1[18]);
-
-                try
-                {
-                    using (StreamWriter writer = File.AppendText(frmTerminal.fileName))
-                    {
-                        // writer.WriteLine("{0},{1},{2},{3},{4}, {5},{6}", MeterData.year, MeterData.day, MeterData.Hour, MeterData.Min, MeterData.Sec, MeterData.data4[2]);
-                        //  writer.WriteLine(Convert.ToString(MeterData.dataTime), ",",Convert.ToString(MeterData.data4[2]),",");
-                        writer.WriteLine(myString);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.ToString());
-                }
-            }
-        }
-
-        */
 
         private void UpdateTimeText()
         {
@@ -2766,8 +2661,7 @@ namespace SerialPortTerminal
             // InitRelaySwitches();
             // InitControlSwitches();
 
-            // config()  this loads config data from file or database. for now it is hard coded
-            // open serial port to meter
+//  read config and cal files and set before autostart
 
             // create temp variables for startup
             aCrossPeriod = ConfigData.crossPeriod;// aCrossPeriod = perX
@@ -2793,12 +2687,13 @@ namespace SerialPortTerminal
             sendCmd("Update Cross Coupling Values"); // download CC parameters 8     -----
 
             ControlSwitches.controlSw = 0x08;       // ControlSwitches.RelayControlSW = 0x08;
-
+            ControlSwitches.DataCollection(enable);
             sendCmd("Send Control Switches");       // 1 ----
-            sendCmd("Send Control Switches");       // 1 ----
+          //  sendCmd("Send Control Switches");       // 1 ----
 
             RelaySwitches.relaySW = 0xB1;           // cmd 0
             sendCmd("Send Relay Switches");         // 0 ----
+            
             ControlSwitches.controlSw = 0x08;       //ControlSwitches.RelayControlSW = 0x08;
             sendCmd("Send Control Switches");       // 1 ----
 
@@ -4133,30 +4028,33 @@ namespace SerialPortTerminal
 
                 //    RelaySwitches.RelaySwitchCalculate();// 0x80
                 RelaySwitches.relaySW = 0x80;// cmd 0
+                ControlSwitches.DataCollection(enable);
                 sendCmd("Send Relay Switches");           // 0 ----
                 sendCmd("Set Cross Axis Parameters");      // download platform parameters 4 -----
                 sendCmd("Set Long Axis Parameters");       // download platform parametersv 5 -----
                 sendCmd("Update Cross Coupling Values");   // download CC parameters 8     -----
 
-                ControlSwitches.controlSw = 0x08; // ControlSwitches.RelayControlSW = 0x08;
+              //  ControlSwitches.controlSw = 0x08; // ControlSwitches.RelayControlSW = 0x08;
 
                 sendCmd("Send Control Switches");           // 1 ----
-                sendCmd("Send Control Switches");           // 1 ----
+             //   sendCmd("Send Control Switches");           // 1 ----
                 ////////////////////////////////////////////////////////////////////////////////////
                 // relay and control switches
                 RelaySwitches.relaySW = 0xB1;// cmd 0
                 sendCmd("Send Relay Switches");           // 0 ----
-                ControlSwitches.controlSw = 0x08; //ControlSwitches.RelayControlSW = 0x08;
-                sendCmd("Send Control Switches");           // 1 ----
+           //     ControlSwitches.controlSw = 0x08; //ControlSwitches.RelayControlSW = 0x08;
+           //     sendCmd("Send Control Switches");           // 1 ----
                                                             ////////////////////////////////////////////////////////////////////////////////////
                 RelaySwitches.relaySW = 0x81;// cmd 0
                 sendCmd("Send Relay Switches");           // 0 ----
-                ControlSwitches.controlSw = 0x08;// ControlSwitches.RelayControlSW = 0x08;
-                sendCmd("Send Control Switches");           // 1 ----
+          ////      ControlSwitches.controlSw = 0x08;// ControlSwitches.RelayControlSW = 0x08;
+          //      sendCmd("Send Control Switches");           // 1 ----
                 RelaySwitches.relaySW = 0x80;// cmd 0
                 sendCmd("Send Relay Switches");           // 0 ----
                 RelaySwitches.relaySW = 0x81;// cmd 0
                 sendCmd("Send Relay Switches");           // 0 ----
+                ControlSwitches.DataCollection(enable);
+                sendCmd("Send Control Switches");           // 1 ----
             }
             else
             {
